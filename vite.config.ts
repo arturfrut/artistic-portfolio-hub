38388bc9e29f19e369b37254ce3893f9b3dca758
arturfrut <arturfruttero@gmail.com>
2026-02-02 19:44:12 -0300

    configs for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: '/artistic-portfolio-hub/'
+  base: process.env.NETLIFY ? '/' : '/artistic-portfolio-hub/'
 })
